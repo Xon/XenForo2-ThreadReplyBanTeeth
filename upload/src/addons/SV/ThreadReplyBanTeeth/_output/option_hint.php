392db888e7caaa2242552bdb69bf04f71fc785a3
Xon <635541+Xon@users.noreply.github.com>
2024-11-22 09:48:57 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/ThreadReplyBanTeeth/_output/option_hint.php
@@ -0,0 +1,26 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property bool|null $svDeleteReplyBan Ban deletes on thread reply ban
+ * @property bool|null $svEditReplyBan Ban edits on thread reply ban
+ * @property bool|null $svLikeReplyBan Ban reactions on thread reply ban
+ * @property bool|null $svReplyBanBanner Display thread reply banned banner on user's posts
+ * @property bool|null $svReplyBanList Display user's thread reply ban list when viewing account details
+ * @property bool|null $svReplyBanReplyBan Ban thread reply bans on thread reply ban
+ * @property bool|null $svThreadReplyBan Ban thread changes on thread reply ban
+ * @property bool|null $svThreadmarkReplyBan Ban threadmark changes on thread reply ban
+ * @property bool|null $svWarnReplyBan Ban warnings on thread reply ban
+ */
+class Options
+{
+}
